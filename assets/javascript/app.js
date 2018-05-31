729132acb459ea24e05a649bcb9386c40aba5973
Candice <candice.schramm@gmail.com>
2018-05-30 21:47:55 -0600

    updated submission, when you click the image or div, it animates or freezes the gif dynamically

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ var ratingContainer;
 $(document).ready(function () {
     renderButtons();;
     console.log("pageloaded");
-});
+
 
 
 function renderButtons() {
@@ -42,47 +42,44 @@ function displayGifs() {
         for (i = 0; i < response.data.length; i++) {
             //make containers for result, gif, rating
             resultContainer = $("<div>")
-            resultContainer.data("imgURL", "animated");
-            newGifContainer = $("<img>")
             ratingContainer = $("<p>").text("Rating: " + response.data[i].rating);
-            //sets still and animated equal to the appropriate url
-            still = response.data[i].images.original_still.url;
-            animated = response.data[i].images.original.url
-
-            //check if data att === still or animated and put appropriate url for Gif inside newGifContainer
-            if (resultContainer.data("imgURL") === "still") {
-                console.log("still");
-                $(newGifContainer).attr("src", still);
-            } else if (resultContainer.data("imgURL") === "animated") {
-                console.log("animated");
-                $(newGifContainer).attr("src", animated);
-            };
-
+            newGifContainer = $("<img>")
+            newGifContainer.addClass("gif")
+            newGifContainer.attr("data-img-type", "still");
+            newGifContainer.attr("data-still-url", response.data[i].images.original_still.url);
+            newGifContainer.attr("data-animated-url", response.data[i].images.original.url)
+            newGifContainer.attr("src", $(newGifContainer).attr("data-still-url"));
             resultContainer.append(newGifContainer);
             resultContainer.prepend(ratingContainer);
             $(".gif-container").append(resultContainer);
         }
     });
-
 }
-// when you click on the resultContainer it changes the data attr imgURL
-$(document).click(resultContainer, function () {
-    //if data att === "still"
-    if (resultContainer.data("imgURL") === "still") {
+
+// when you click on the gif/image
+$(document).on("click", ".gif", function () {
+    //if imgType att === "still"
+
+    if ($(this).attr("data-img-type") === "still") {
+        //animate the picture
+        console.log( $(this).data("animated-url"))
+        $(this).attr("src", $(this).data("animated-url"));
+        console.log("hello2")
+        console.log($(this));
+      
         //change value to animated
-        $(newGifContainer).data("imgURL", "animated");
-        //else if it's already set to animated
-    } else if (resultContainer.data("imgURL") === "animated") {
+        $(this).attr("data-img-type", "animated");
+    } else {
+        $(this).attr("src", $(this).data("still-url"));
         //change it to still
-        $(newGifContainer).data("imgURL", "still");
+        $(this).data("data-img-url", "still");
     };
-    displayGifs();
 });
 
 //the imageURL for that data-val div will change to animated
 
 //when you click on a button, it displays the gifs for you
-$(document).on("click", ".animal-button", function(){
+$(document).on("click", ".animal-button", function () {
     animal = $(this).attr("data-animal")
     displayGifs()
 });
@@ -95,6 +92,7 @@ $(document).on("click", ".add-button", function () {
     renderButtons();
 });
 
+});
 
 
 
